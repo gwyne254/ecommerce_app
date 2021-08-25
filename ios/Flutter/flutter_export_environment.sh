2f9eb9df97e7b3a8117d8ef7fea45df409d674fe
gwyne254 <gwyneofficial@gmail.com>
2021-08-25 14:13:49 +0000

    Changes made to Flutter Environent

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/george/Desktop/myFlutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/george/Desktop/Project/UI Designs/Flutter/eCommerceDesigns/Flutter-Shopping-UI-Kit"
+export "FLUTTER_APPLICATION_PATH=/home/george/Desktop/Project/ecommerce_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
